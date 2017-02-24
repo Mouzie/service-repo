92c939125daec839275c7e7501b6136b113f04c7
mouzie <moses.mathebula16@gmail.com>
2017-02-24 14:58:40 +0200

    Calculate service commit

--- test-service/src/main/java/za/co/reverside/test/Service.java
+++ test-service/src/main/java/za/co/reverside/test/Service.java
@@ -6,6 +6,9 @@ import java.util.Date;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+
 @RestController
 public class Service{
 
@@ -17,5 +20,15 @@ public class Service{
 	@RequestMapping(path="api/java/time")
 	public String getTime(){
 		return new Date().toString();
+	}
+
+	@RequestMapping(path="api/java/calculate" , method=RequestMethod.GET )
+	public int calc(){
+		int a,b,c;
+		a =  10;
+		b = 20;
+		c = a + b;
+
+		return c;
 	}
 }
